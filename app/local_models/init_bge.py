535d44bc86d2ab44e44107b88c81354f08658bd6
wangwj <wangweijun@aeromedia.cn>
2025-05-24 16:09:51 +0800

    初始化

--- app/local_models/init_bge.py
+++ app/local_models/init_bge.py
@@ -1,5 +1,14 @@
 from sentence_transformers import SentenceTransformer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 if __name__ == '__main__':
-    model = SentenceTransformer("BAAI/bge-small-zh")
-    model.save("./bge-small-zh")
+    # BAAI/bge-small-zh
+    # model = SentenceTransformer("BAAI/bge-small-zh")
+    # model.save("./bge-small-zh")
+
+    model_name = "Qwen/Qwen2.5-VL-3B-Instruct"
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", trust_remote_code=True).eval()
+    # 保存模型和分词器
+    model.save_pretrained("./Qwen2.5-VL-3B-Instruct")
+    tokenizer.save_pretrained("./Qwen2.5-VL-3B-Instruct")
